5cbddb55a7cfdff290f817ac29d67a8d45519282
ravi11o <ravi.suraj110@gmail.com>
2022-06-16 13:06:02 +0530

    operations on array

--- block-BJaace/code/index.js
+++ block-BJaace/code/index.js
@@ -1,31 +1,44 @@
 // 1. Declare an empty array and store it in a variable.
+let array=[];
 
 // 2. Now check the type of empty array you declared above.
+typeof array
+// is "object"
 
 // 3. Create an array named `colors` that contains five different names of colors as strings.
+let colors=["blue","green","white","black","orange"]
 
 // 4. Access the first color in the array and print it to the console using `console.log()`
+colors[0],console.log(colors[0])
 
 // 5. Access the third color in the array and print it to the console using `console.log()`
+colors[2],console.log(colors[2])
+
 
 // 6. Access the last color in the array and print it to the console using `console.log()`
+colors[colors.length],console.log([colors.length])
 
 // 7. Write one line of code that changes the value of the first color in the list to "black" (overwriting the previous value).
-
+colors[0]="black"
 // 8. Write one line of code that changes the value of the last color in the list to "ultraviolet" (overwriting the previous value).
-
+colors[4]="ultavoilet"
 // 9. Now, Find the length of the array named `colors`.
+colors["length"]
 
 // 10. Create a new variable called fourthColor and set it equal to the fourth color in the list.
+let fourthColor=['red']
+colors[3]='red'
 
 // 11. Add another color to the end of the list.
+colors[colors.length]="yellow"
 
 // 12. Add another color to the beginning of the list (use array method array.unshift('gray'))
 
 // 13. Remove the first color from the starting of the array
+delete colors[0]
 
 // 14. Remove the last color from the end of list, and then print the length of the array to the console one more time.
-
+delete colors[colors.length]
 // 15. Remove the second color from the end of list.
 
 // 16. Write a for loop to iterate through every element in the array and print each element to the console.
